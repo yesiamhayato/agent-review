c809f593e116304e23c02e359e3ec78d90baaf15
yesiamhayato <njusa18endo@gmail.com>
2020-12-23 18:41:27 +0900

    編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :agents, only: [:index, :show] do
-    resources :reviews, only: [:index, :create, :edit]
+    resources :reviews, only: [:index, :create, :edit, :update]
   end
 end
